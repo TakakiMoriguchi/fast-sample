de3cbe856753f4e8da5ea5818026152fdc434f71
TakakiMoriguchi <entersight@gmail.com>
2024-02-01 09:48:35 +0900

    first

---
+++ api/schemas/task.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+
+class TaskBase(BaseModel):
+    title: str | None = Field(None, example="クリーニングを取りに行く")
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Task(BaseModel):
+    id: int
+    done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
